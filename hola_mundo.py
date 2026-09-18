42c6051cfbaf80e94b25fa419ad55db39d2171fc
Pedro <pedro.victoria@campus.xaloc.org>
2026-09-18 09:31:14 +0200

    Subo la cuarta versión del archivp hola_mundo.py
    En esta cuarta versión he añadido un comentario con el enunciado del ejercicio.

--- hola_mundo.py
+++ hola_mundo.py
@@ -1,3 +1,4 @@
+#Los alumnos crearán y configurarán una cuenta en GitHub para desarrollar un repositorio personal, subir y versionar archivos de código, y documentar el proceso mediante capturas de pantalla y un README explicativo. La actividad culmina con la entrega de un informe en Word que incluye evidencias del repositorio, historial de commits y respuestas reflexivas sobre el uso de GitHub como herramienta de programación colaborativa.
 #Este es un comentario para una nueva versión
 print("Hola mundo")
 #Esta es la tercera versión del archivo hola_mundo.py donde añado mi nombre.
